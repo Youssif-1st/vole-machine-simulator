81f5b2ed1efd4f408d25bd2d5e8a9b49dec980af
Youssif-1st <offabasha@gmail.com>
2024-11-07 03:27:30 +0200

    upload final project

--- Memory.cpp
+++
@@ -1,9 +0,0 @@
-#include "Memory.h"
-
-string Memory::getCell(int address) const {
-    return memory[address];
-}
-
-void Memory::setCell(int address, const string& value) {
-    memory[address] = value;
-}
--- Register.cpp
+++
@@ -1,9 +0,0 @@
-#include "Register.h"
-
-string Register::getCell(int index) const {
-    return memory[index];
-}
-
-void Register::setCell(int index, const string& value) {
-    memory[index] = value;
-}
